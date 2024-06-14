fe959b74e6e20419fa3539e9c210a8dc8e9ee3ec
Quinten Kamphuis <163667554+quinten-kamphuis@users.noreply.github.com>
2024-06-14 14:15:43 +0200

    Add button to add and delete books from library

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const tableContainer = document.querySelector('.table-container');
 
+const confirmBtn = addBookDialog.querySelector("#confirmBtn");
+
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
@@ -21,18 +23,44 @@ Book.prototype.info = function() {
 };
 
 Book.prototype.displayInTable = function() {
-    tableContainer.innerHTML += `<tr>
+    return `<tr>
         <td>${this.title}</td>
         <td>${this.author}</td>
         <td>${this.pages}</td>
         <td>${this.read ? 'Yes' : 'No'}</td>
     </tr>`;
 };
-    
-const addBookToLibrary = () => {
-    
+
+const addDeleteEvents = () => {
+    document.querySelectorAll(".delete-book")
+        .forEach(btn => btn.addEventListener("click", (event) => {
+            const id = btn.parentElement.parentElement.id;
+            myLibrary.splice(id, 1);
+            displayAllBooks();
+    }));
 };
 
-myLibrary.forEach(book => {
-    book.displayInTable();
+const displayAllBooks = () => {
+    tableContainer.innerHTML = '';
+    myLibrary.forEach((book, index) => {
+        const row = document.createElement('tr');
+        row.id = index;
+        row.innerHTML = book.displayInTable();
+        row.innerHTML += `<td><img class="delete-book" src="img/trash-alt-svgrepo-com.svg" height="30" alt="Trash Can"></td>`;
+        tableContainer.appendChild(row);
+    });
+    addDeleteEvents();
+};
+
+const addBookToLibrary = (title, author, pages, read) => {
+    const newBook = new Book(title, author, pages, read);
+    myLibrary.push(newBook);
+    displayAllBooks();
+};
+
+confirmBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+    addBookToLibrary(title.value, author.value, pages.value, read.checked);
 });
+
+displayAllBooks();
